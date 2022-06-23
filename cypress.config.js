f11fbe84a098835770a55efcd751a234b27fc8b9
Karel Daneš <karel.danes@gmail.com>
2022-06-23 20:32:41 +0200

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    baseUrl: 'https://boostbrothers-bikes-api.herokuapp.com/motorcycles/'
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family == 'chromium') {
+          launchOptions.args.push('--auto-open-devtools-for-tabs')
+          return launchOptions;
+        }
+      })
     },
   },
 });
--- cypress/e2e/api-call-I.cy.js
+++ cypress/e2e/api-call-I.cy.js
@@ -1,5 +1,25 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('try max from rquest',() => {
+  it('step 1 (body lenght)', () => {
+    cy
+      .request({
+        method: "GET",
+        url: Cypress.env('baseUrl')
+      }).then((resp) => {
+        expect(resp.body).to.exist
+        expect(resp.body).to.have.length(6)
+      })
+  })
+
+  it('step 2 (status code)', () => {
+    cy
+      .request({
+        method: "GET",
+        url: Cypress.env('baseUrl')
+      }).then((resp) => {
+        console.table(resp.body)
+        expect(resp.status).to.eq(200)
+      })
   })
 })

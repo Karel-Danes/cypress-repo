631261973547e2a97fc5bd6c03a99850caf6b3b4
Karel Daneš <karel.danes@gmail.com>
2022-07-13 11:40:53 +0200

    pumping on

--- cypress/e2e/api-call-round-1.cy.js
+++ cypress/e2e/api-call-round-1.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
+const fixtures = require('../fixtures/fixtures')
 
-describe('Testing API expected beavior',() => {
+describe('Testing API expected behavior', () => {
   it('step 1 (body lenght)', () => {
+
+    cy.intercept('/login', (req) => {
+      req.headers['x-custom-headers'] = 'added by expando'
+      console.log(req)
+    })
+      .as('headers')
+
+    /*
+  cy
+    .wait('@headers')
+    .then((resp) => {
+      console.log(resp)
+    })
+
+    */
     cy
       .request({
         method: "GET",
@@ -19,7 +35,48 @@ describe('Testing API expected beavior',() => {
         url: Cypress.env('baseUrl')
       }).then((resp) => {
         console.table(resp.body)
-        expect(resp.status).to.eq(200)
+        expect(resp.status).to.be.oneOf(fixtures.code.twoXX)
+      })
+      
+  })
+  it('step 3 (query parameter behaviour)', () => {
+    cy
+      .request({
+        method: 'GET',
+        url: Cypress.env('baseUrl'),
+        qs: {
+          q: 'triumph'
+        }
+      }).then((resp) => {
+
+        expect(resp.body).to.have.length(1)
       })
   })
+
+  it('step 4 (custom headers pump in)', () => {
+
+    cy
+      .request({
+        method: 'GET',
+        url: Cypress.env('baseUrl'),
+        headers: {
+          "x-custom-headers": 'rock and roll'
+
+        },
+      }).then((res) => {
+        //console.log(typeof (resp.headers))
+        console.log(res.headers)
+      })
+
+
+    // the application makes the call ...
+    // confirm the custom header was added
+    /*
+    cy.wait('@headers')
+      .its('request.headers')
+      .should('have.property', 'x-custom-headers', 'added by cy.intercept')
+*/
+
+  })
+
 })
